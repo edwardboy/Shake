7dd513ed79e8bc8278ec23de2dbce8a861772220
gyh <1254339809@qq.com>
2020-09-02 20:42:49 +0800

    update gitignore

--- Pods/Target Support Files/Pods-TestiOS-TestiOSUITests/Pods-TestiOS-TestiOSUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestiOS_TestiOSUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TestiOS_TestiOSUITests
-@end
--- Pods/Target Support Files/Pods-TestiOSTests/Pods-TestiOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestiOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_TestiOSTests
-@end
